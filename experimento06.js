83b9f5a3147e2010757b1cf479064567272b92ae
hiagoborzuk <hiago.urbanski@escola.pr.gov.br>
2026-08-27 11:45:50 +0000

    heitor

--- experimento06.js
+++ experimento06.js
@@ -3,14 +3,14 @@ console.log(botaoAbrir)
 const modalContainer = document.getElementById("modalContainer");
 
 botaoAbrir.addEventListener("click", function(){
-    modalContainer.innerHTML = 
+    modalContainer.innerHTML = `
             <div class="modal-fundo">
                 <div class="modal-caixa">
                     <h2>Aviso!</h2>
                     <p> Este modal inteiro foi criado pelo javaScript.</p>
                     <button id="btnFechaModal">Fecha</button>
                 </div>
-            </div>;
+            </div> `;
 
             const botaoFechar = document.getElementById("btnFechaModal");
                 botaoFechar.addEventListener("click", function(){
